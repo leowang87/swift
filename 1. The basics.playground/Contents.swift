efdf6d7e67e53d96235f9ec43735fd69d58ee4d2
Liang Wang <leowang87@gmail.com>
2017-01-19 22:22:45 +0100

    1. The Basics

---
+++ 1. The basics.playground/Contents.swift
@@ -0,0 +1,68 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var str = "Hello, playground"
+
+//The Basics
+// Declaring constants and variables
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+// Declaring multiple constants or multiple variables on a single line
+
+var x = 0.0, y = 0.0, z = 0.0
+
+// Type Annotations
+var welcomeMessage: String
+welcomeMessage = "Hello"
+print(welcomeMessage)
+
+// Nmaing multiple variables
+var red, green, blue: Double
+
+// Naming constants and variables
+let pi = 3.14159
+print(pi)
+
+// Change the value of an existing variable
+var friendlyWelcome = "Hello"
+friendlyWelcome = "Bonjour"
+
+let languageName = "Swift"
+// languageName = "Swift++"
+
+// Printing constants and variables
+
+print(friendlyWelcome)
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+// Comments
+// This is a comment.
+
+/* this is also a comment
+ but is written over multiple lines. */
+
+// Semicolons required if write multiple separate statements in a single line
+let cat = "cat"; print(cat)
+
+// Integers
+let minValue8 = UInt8.min
+let maxValue8 = UInt8.max
+
+let minValue32 = UInt32.min
+let maxValue32 = UInt32.max
+
+let minValue64 = UInt64.min
+let maxValue64 = UInt64.max
+
+// Floating-point numbers
+print(1.25e2)
+print(1.25e-2)
+
+
+
+
+
+
