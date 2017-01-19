29c0f8d2233c1fa0b2ad37741c01e86b85751616
Liang Wang <leowang87@gmail.com>
2017-01-19 21:47:30 +0100

    a swift tour - more examples

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -69,3 +69,68 @@ let emptyArray = [String]()
 let emptyDictionary = [String:Float]()
 
 
+// Control Flow
+// Use if and switch to make conditionals
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in individualScores{
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+
+// Optional String
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+// Provide a default value using the ?? operator
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+print(informalGreeting)
+
+// Switch
+//let vegetable = "red pepper"
+let vegetable = "cucumber"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+default:
+    print("Everything tastes good in soup.")
+}
+
+// for-in
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+
+// find the larget number
+var larget = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > larget {
+            larget = number
+        }
+    }
+}
+
+print(larget)
+
+// find which kind of number was the larget and what the larget number was.
+ 
