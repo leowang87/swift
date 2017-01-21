6aa89998703a01c81e2a996ac418ba18af6921a2
Liang Wang <leowang87@gmail.com>
2017-01-21 23:31:22 +0100

    Done with 1. The basics

--- 1. The basics.playground/Contents.swift
+++ 1. The basics.playground/Contents.swift
@@ -62,7 +62,99 @@ print(1.25e2)
 print(1.25e-2)
 
 
+// Booleans
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+
+let i = 1
+//if i {
+    // this example will not compile, and will report an error.
+//}
+
+if i == 1 {
+    // this example will compile successfully
+}
 
 
 
+// Tuples
+let http404Error = (404, "Not Found")
+// http404Error is of type (Int, String), and equals (404, "Not Found")
 
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+
+// ignore parts of the tuple with an underscore(_)
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+// access the individual element values in a tuple using index numbers starting at zero
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+// name the individual elements in a tuple when the tuple is defined
+
+let http200Status = (statusCode: 200, description: "OK")
+// use the element names to access the values of thoese elements:
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
+
+
+// Optionals
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+
+var serverResponseCode: Int? = 404
+// serverResponseCode contains an actual Int value of 404
+print(serverResponseCode)
+
+serverResponseCode = nil
+// serverResponseCode now contains no value
+print(serverResponseCode)
+
+var surveyAnswer: String?
+print(surveyAnswer)
+
+// check if the converted number contains value
+if convertedNumber != nil {
+    print("convertedNumber contains some integer value.")
+}
+
+// forced-unwrapping, "!"
+if convertedNumber != nil {
+    print("convertedNumber has an integer value of \(convertedNumber!).")
+}
+
+// optional binding
+if let actualNumber = Int(possibleNumber) {
+    print("\"\(possibleNumber)\" has an integer value of \(actualNumber)")
+} else {
+    print("\"\(possibleNumber)\" could not be converted to an integer")
+}
+
+// Error Handling
+/*
+func makeASandwich() throws {
+    // ...
+}
+
+do {
+    try makeASandwich()
+    eatASandwich()
+} catch SandwichError.outOfCleanDishes {
+    washDishes()
+} catch SandwichError.missingIngredients {
+    buyGroceries(ingredients)
+}
+ */
+
+// Assertions
+let age = -3
+// assert(age >= 0, "A person's age cannot be less than zero")
